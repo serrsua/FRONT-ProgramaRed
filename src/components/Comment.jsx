02c9d86563b22e94ec174d6a0499f75d4b2473c9
Ailín <ailingalanterosso@gmail.com>
2023-04-18 02:46:35 -0300

    delete comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,38 @@
-const Comment = ({ comment, user }) => {
+import Swal from "sweetalert2";
+import axios from "axios";
+
+const Comment = ({ comment, user, userId, id, postId }) => {
+
+    const deleteComment = async () => {
+        Swal.fire({
+          title: "¿Estás seguro?",
+          text: "No puedes revertir esta acción",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          cancelButtonText: "Cancelar",
+          confirmButtonText: "Confirmar",
+        }).then(async (result) => {
+          if (result.isConfirmed) {
+            await axios.delete("/comments", { data: {id: Number(id), postId: Number(postId)}});
+            await Swal.fire({
+              title: "Comentario borrado",
+              icon: "success",
+              timer: 1200,
+            });
+            window.location.reload()
+          }
+        });
+      };
+
     return(
         <div>
             <h1>{user.username} dice:</h1>
             <p>{comment}</p>
+            <div>
+            {Number(user.id) === Number(userId) && <button onClick={deleteComment}>eliminar </button>}
+            </div>
         </div>
     )
 }
