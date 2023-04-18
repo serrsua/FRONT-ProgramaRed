4e1e0795d303ae76cf52345ef31feb95de4c2bc9
franco <91661089+davidfranncoo@users.noreply.github.com>
2023-04-18 13:36:38 -0300

    register/login

--- src/components/SignUp/FormSingUp.jsx
+++ src/components/SignUp/FormSingUp.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import axios from "axios";
 import { NavLink, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import img from "../../images/fondoregistro_adobe_express.svg";
 
 const FormSignUp = () => {
   const [form, setForm] = useState({
@@ -46,18 +47,18 @@ const FormSignUp = () => {
     e.preventDefault();
     try {
       const info = await axios.post("/register", form);
-      const response = await axios.post('/subcriptionsEmail', {
-        username:form.username, 
-        email:form.email,
-        type:"Registro"  
-      })
+      const response = await axios.post("/subcriptionsEmail", {
+        username: form.username,
+        email: form.email,
+        type: "Registro",
+      });
       Swal.fire({
         icon: "success",
         title: info.data,
         text: response.data,
         showConfirmButton: false,
         timer: 1600,
-      })
+      });
       clearForm();
       navigate("/");
     } catch (error) {
@@ -66,86 +67,97 @@ const FormSignUp = () => {
         title: error.response.data.error,
         showConfirmButton: false,
         timer: 2000,
-      })
+      });
     }
   };
 
   return (
-    
-    <div className="DIV_FORM_SIGN p-3 lg:col-span-4 md:my-6 transition-all">
-      <div className="flex flex-col max-w-md mx-auto p-6 bg-white rounded-md shadow-md">
-        <h3 className=" self-center text-xl font-bold text-green-700 mb-6">Registrarse</h3>
-        <p className=" self-center">
-          Ya tienes una cuenta logeate <NavLink className=" font-bold text-green-800" to="/">aquí</NavLink>
-        </p>
-        <form
-          className=" gap-4
+    <div className="DIV_FORM_SIGN absolute flex items-center ">
+      <img className="brightness-50 w-screen h-screen object-cover" src={img} />
+
+      <div className="  absolute top-20 left-0 w-full text-center text-lg md:text-xl text-black  opacity-70 shadow-xl font-mediun tracking-widest">
+        <div className="flex flex-col max-w-md mx-4 sm:mx-auto p-6 bg-slate-400 rounded-md shadow-md ">
+          <h3 className=" self-center text-3xl font-bold text-green-700 mb-6">
+            Registrarse
+          </h3>
+          <p className=" self-center mb-10">
+            Si tienes una cuenta logeate{" "}
+            <NavLink className=" font-bold text-green-700" to="/">
+              aquí
+            </NavLink>
+          </p>
+          <form
+            className=" gap-4
          flex flex-col justify-center"
-          onSubmit={handleSubmit}
-        >
-          <div className="flex flex-col ">
-            <label>Nombre de usuario: </label>
-            <input
-              onChange={changeHandler}
-              placeholder="Escribe tu nombre de usuario..."
-              type="text"
-              name="username"
-              value={form?.username}
-              className={`border-gray-300 block w-full px-2 py-1 rounded-md shadow-sm focus:outline-none focus:ring-2 transition duration-150 ease-in-out ${
-                errors.username
-                  ? "focus:border-red-500 focus:ring-red-500"
-                  : "focus:ring-green-500 focus:border-green-500"
-              }
-              `}
-            />
-            <span className="text-red-500 text-sm">{errors?.username}</span>
-          </div>
-
-          <div className="flex flex-col ">
-            <label>Email: </label>
-            <input
-              onChange={changeHandler}
-              placeholder="Escribe tu email..."
-              type="text"
-              name="email"
-              value={form?.email}
-              className={`border-gray-300 block w-full px-2 py-1 rounded-md shadow-sm focus:outline-none focus:ring-2 transition duration-150 ease-in-out ${
-                errors.email
-                  ? "focus:border-red-500 focus:ring-red-500"
-                  : "focus:ring-green-500 focus:border-green-500"
-              }
-              `}
-            />
-            <span className="text-red-500 text-sm">{errors?.email}</span>
-          </div>
-
-          <div className="flex flex-col ">
-            <label>Contraseña: </label>
-            <input
-              onChange={changeHandler}
-              type="password"
-              name="password"
-              value={form?.password}
-              className={`border-gray-300 block w-full px-2 py-1 rounded-md shadow-sm focus:outline-none focus:ring-2 transition duration-150 ease-in-out ${
-                errors.password
-                  ? "focus:border-red-500 focus:ring-red-500"
-                  : "focus:ring-green-500 focus:border-green-500"
-              }
-              `}
-            />
-            <span className="text-red-500 text-sm">{errors?.password}</span>
-          </div>
-
-          <button
-            className="flex self-center bg-mediumGreen text-white font-bold py-2 px-4 rounded"
-            type="submit"
+            onSubmit={handleSubmit}
           >
-            Enviar
-          </button>
-        </form>
+            <div className="flex flex-col font-thin ">
+              <label className="flex items-start sm:text-sm md:text-lg  ">
+                Nombre de usuario:{" "}
+              </label>
+              <input
+                onChange={changeHandler}
+                placeholder="Escribe tu nombre de usuario..."
+                type="text"
+                name="username"
+                value={form?.username}
+                className={`  placeholder-slate-500 bg-slate-300 border-gray-300 block w-full px-2 py-1 rounded-md shadow-white focus:outline-none focus:ring-2 transition duration-150 ease-in-out ${
+                  errors.username
+                    ? "focus:border-red-500 focus:ring-red-500"
+                    : "focus:ring-green-700 focus:border-green-700"
+                }
+              `}
+              />
+              <span className="text-red-500 text-sm">{errors?.username}</span>
+            </div>
+
+            <div className="flex flex-col font-thin  ">
+              <label className="flex items-start md:text-lg  ">Email: </label>
+              <input
+                onChange={changeHandler}
+                placeholder="Escribe tu email..."
+                type="text"
+                name="email"
+                value={form?.email}
+                className={`placeholder-slate-500 bg-slate-300 border-gray-300 block w-full px-2 py-1 rounded-md shadow-sm focus:outline-none focus:ring-2 transition duration-150 ease-in-out ${
+                  errors.email
+                    ? "focus:border-red-500 focus:ring-red-500"
+                    : "focus:ring-green-700 focus:border-green-700"
+                }
+                `}
+              />
+              <span className="text-red-500 text-sm">{errors?.email}</span>
+            </div>
+
+            <div className="flex flex-col font-thin mb-10 ">
+              <label className="flex items-start md:text-lg  ">
+                Contraseña:{" "}
+              </label>
+              <input
+                onChange={changeHandler}
+                type="password"
+                name="password"
+                value={form?.password}
+                className={`placeholder-slate-500 bg-slate-300 border-gray-300 block w-full px-2 py-1 rounded-md shadow-sm focus:outline-none focus:ring-2 transition duration-150 ease-in-out ${
+                  errors.password
+                    ? "focus:border-red-500 focus:ring-red-500"
+                    : "focus:ring-green-700 focus:border-green-700"
+                }
+              `}
+              />
+              <span className="text-red-500 text-sm">{errors?.password}</span>
+            </div>
+
+            <button
+              className="flex self-center bg-mediumGreen text-white font-bold py-2 px-10 rounded-full"
+              type="submit"
+            >
+              Enviar
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-  
   );
 };
 
--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -2,45 +2,63 @@ import React from "react";
 import logo from "../images/logo.png";
 import Login from "../components/Login/Login"
 import { NavLink } from "react-router-dom";
+import video from "../images/Video2.mp4";
+
 
 const Landing = () => {
   return (
-    <div className="DIV_LANDING grid p-3 bg-slate-400 lg:col-span-2 md:my-6 transition-all">
-      {/* Hero Section */}
-      <div className="w-full bg-center bg-cover">
-        <div className="flex items-center justify-center h-full text-center">
-          <div className="text-white p-2 ">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4">
-              ProgramaRed
-            </h1>
-          <h2 className="text-4xl font-bold mb-4">
-             ¡Conecta con otros programadores!
-          </h2>
-            <p className="text-lg md:text-xl">
-              Comparte conocimientos, colabora en proyectos y mucho más.
-            </p>
-          </div>
-        </div>
+    <div className=" relative col-span-2 flex items-center">
+        <video className="brightness-50   w-screen h-screen object-cover" src={video} autoPlay loop muted></video>
+      <div>
+        <h1 className="absolute top-0 left-0 w-full font-sans text-center text-4xl md:text-6xl text-green-600 font-bold tracking-widest">
+         
+          ProgramaRed
+        </h1>
+       
       </div>
-
-      {/* Main Content */}
-      <div className="max-w-3xl w-full px-4">
-        <div className="flex flex-col md:flex-row items-center justify-center pt-4">
-          <div className="flex flex-col items-center mb-4 md:mb-0">
-            <img
-              src={logo}
-              alt="logo"
-              className="bg-ligthGreen rounded-full w-24 h-24 p-2 animate-spin-slow"
-            />
-            <NavLink to="/about" >
-              <h2 className="text-lg font-bold px-5 py-3 text-gray-800 mt-4 hover:text-white hover:bg-slate-900 rounded-md p-1 transition-all">¿Qué es ProgramaRed?</h2>
-            </NavLink>
-          </div>
+      {/* Hero Section */}
+      <div className="fixed DIV_LANDING w-[80%]  grid p-4 bg-slate-400 bg-opacity-50 rounded-md lg:col-span-2 md:my-6 transition-all top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      {/* <div className="relative DIV_LANDING  w-1/2 grid p-3 bg-slate-400 bg-opacity-50 rounded-md lg:col-span-2 md:my-6 transition-all"> */}
+        <div className="text-white p-2 ">
+          {/* <h1 className="text-4xl md:text-6xl font-bold mb-4">
+              ProgramaRed
+            </h1> */}
+          {/* <h2 className="text-4xl font-bold mb-4">
+             ¡Conecta con otros programadores!
+          </h2> */}
+          {/* <p className="text-lg md:text-xl">
+              Comparte conocimientos, colabora en proyectos y mucho más.
+            </p> */}
+        </div>
+        <div className="w-full bg-center bg-cover">
+          <div className="flex items-center justify-center h-full text-center"></div>
         </div>
 
-        {/* Login Section */}
-        <div className="DIV_LOADING-LOGIN max-w-sm w-full mx-auto">
-          <Login />
+        {/* Main Content */}
+        <div className=" max-w-3xl w-full px-4">
+          <div className=" md:flex-row items-center justify-center pt-4">
+          <h1 className=" absolute top-0 sm:text-ls left-0 w-full mt-2 text-center md:text-2xl  text-black font-md tracking-widest">
+          ¡Conecta con otros programadores!
+        </h1>
+            <div className="flex flex-col items-center mb-4 md:mb-0">
+              <img
+                src={logo}
+                alt="logo"
+                className="bg-ligthGreen rounded-full w-24 h-24 p-2 animate-spin-slow"
+              />
+            </div>
+
+          {/* Login Section */}
+          <div className="DIV_LOADING-LOGIN max-w-sm w-full mx-auto">
+            <Login />
+          </div>
+          
+          <NavLink className="flex justify-center " to="/about">
+            <h2 className="text-lg font-bold  items-center px-5  py-3 text-gray-800 mt-4 hover:text-white hover:bg-slate-900 rounded-md p-1 transition-all">
+              ¿Qué es ProgramaRed?
+            </h2>
+          </NavLink>
+          </div>
         </div>
       </div>
     </div>
