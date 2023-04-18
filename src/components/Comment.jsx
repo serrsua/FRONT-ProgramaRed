966581f6426bdf60d8e29e0fb8f1123a66a4fed0
Ailín <ailingalanterosso@gmail.com>
2023-04-18 19:43:38 -0300

    se puede editar pero hay un bug en la alerta

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -28,6 +28,43 @@ const Comment = ({ comment, user, userId, id, postId, toggleDetails }) => {
     });
   };
 
+  const editHandler = () => {
+    Swal.fire({
+        title: 'Edita tu comentario',
+        input: 'text',
+        inputValue: comment,
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+        confirmButtonText: 'Editar',
+        showLoaderOnConfirm: true,
+        preConfirm: (event) => {
+          return axios.put(`/comments/${id}`, {comment: event})
+            .then(response => {
+              if (!response.ok) {
+                throw new Error(response.statusText)
+              }
+              return response.json()
+            })
+            .catch(error => {
+              Swal.showValidationMessage(
+                `Request failed: ${error}`
+              )
+            })
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: `${result.value.login}'s avatar`,
+            imageUrl: result.value.avatar_url
+          })
+        }
+      })
+  }
+
   return (
     <div className="relative border border-blue-200 py-2 px-1">
       <p className="text-green-800 text-base">
@@ -43,7 +80,8 @@ const Comment = ({ comment, user, userId, id, postId, toggleDetails }) => {
       <p className="text-amber-600 text-sm font-medium">{comment}</p>
       <div className="absolute top-1 right-1">
         {Number(user.id) === Number(userId) && (
-          <button onClick={deleteComment}>
+            <div>
+          <button onClick={editHandler}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -55,10 +93,27 @@ const Comment = ({ comment, user, userId, id, postId, toggleDetails }) => {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
               />
             </svg>
           </button>
+          <button onClick={deleteComment}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#B91C1C"
+            className="w-5 h-5 hover:scale-125"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+            />
+          </svg>
+        </button>
+        </div>
         )}
       </div>
     </div>
