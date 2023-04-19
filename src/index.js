cbc2453ee00d3c6b9538f1801770f4b7be1a9820
Ailín <ailingalanterosso@gmail.com>
2023-04-18 22:09:34 -0300

    error arreglado

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import store from "./redux/store";
 import axios from "axios";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-axios.defaults.baseURL = 'http://localhost:3001';
-// axios.defaults.baseURL = "https://programared-back-production.up.railway.app";
+// axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = "https://programared-back-production.up.railway.app";
 
 const container = document.getElementById('root');
 // Create a root.
